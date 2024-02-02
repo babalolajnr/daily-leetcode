3a83d8e15136267504953c0d71f0ddc24a97b1fb
Abdulqudduus Babalola <babalolajnr@gmail.com>
2024-02-02 16:11:47 +0100

    solution: 02/02/2024

--- rust-leet/src/lib.rs
+++ rust-leet/src/lib.rs
@@ -26,7 +26,7 @@ pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, _n: i32) {
 /// for (int i = 0; i < actualLength; i++) {
 ///     assert nums[i] == expectedNums[i];
 /// }
-/// ````
+/// ```
 /// If all assertions pass, then your solution will be accepted.
 ///
 ///
@@ -54,6 +54,55 @@ pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, _n: i32) {
 pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
     nums.retain(|&x| x != val);
     nums.len() as i32
+}
+
+/// You are given an integer array nums of size `n` and `a` positive integer `k`.
+///
+/// Divide the array into one or more arrays of size 3 satisfying the following conditions:
+///
+/// Each element of nums should be in exactly one array.
+/// The difference between any two elements in one array is less than or equal to `k`.
+/// Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
+///
+///
+/// # Example 1:
+///```
+/// Input: nums = [1,3,4,8,7,9,3,5,1], k = 2
+/// Output: [[1,1,3],[3,4,5],[7,8,9]]
+/// Explanation: We can divide the array into the following arrays: [1,1,3], [3,4,5] and [7,8,9].
+/// The difference between any two elements in each array is less than or equal to 2.
+/// ```
+/// _Note that the order of elements is not important._
+///
+/// # Example 2:
+///```
+/// Input: nums = [1,3,3,2,7,3], k = 3
+/// Output: []
+/// Explanation: It is not possible to divide the array satisfying all the conditions.
+///```
+///
+/// # Constraints:
+///```
+/// n == nums.length
+/// 1 <= n <= 105
+/// n is a multiple of 3.
+/// 1 <= nums[i] <= 105
+/// 1 <= k <= 105
+/// ```
+pub fn divide_array(nums: Vec<i32>, k: i32) -> Vec<Vec<i32>> {
+    let mut sorted_nums = nums;
+    sorted_nums.sort();
+
+    let result: Vec<Vec<i32>> = sorted_nums.chunks(3).map(|chunk| chunk.to_vec()).collect();
+
+    if result
+        .iter()
+        .any(|chunk| chunk.iter().max().unwrap() - chunk.iter().min().unwrap() > k)
+    {
+        return vec![];
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -114,5 +163,15 @@ mod tests {
 
         assert_eq!(5, k);
         assert_eq!(output, nums);
+    }
+
+    #[test]
+    fn divide_array_case_1() {
+        let nums = vec![1, 3, 4, 8, 7, 9, 3, 5, 1];
+        let k = 2;
+
+        let output = vec![vec![1, 1, 3], vec![3, 4, 5], vec![7, 8, 9]];
+
+        assert_eq!(output, divide_array(nums, k))
     }
 }
