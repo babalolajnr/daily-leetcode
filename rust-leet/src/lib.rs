912e2606708815caeb5d94045b82f7c522e4f3d1
Abdulqudduus Babalola <babalolajnr@gmail.com>
2024-02-09 08:59:25 +0100

    solution: 09/02/24

--- rust-leet/src/lib.rs
+++ rust-leet/src/lib.rs
@@ -299,6 +299,53 @@ pub fn string_construction(s: &str) -> i32 {
     s.chars().collect::<HashSet<_>>().len() as i32
 }
 
+/// Given an array nums of size `n`, return the majority element.
+///
+/// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+///
+///
+/// # Example 1:
+///```
+/// Input: nums = [3,2,3]
+/// Output: 3
+/// ```
+/// # Example 2:
+/// ```
+/// Input: nums = [2,2,1,1,1,2,2]
+/// Output: 2
+/// ````
+///
+/// # Constraints:
+///
+/// - `n == nums.length`
+/// - `1 <= n <= 5 * 104`
+/// - `-109 <= nums[i] <= 109`
+///
+/// *Follow-up*: Could you solve the problem in linear time and in O(1) space?
+pub fn majority_element(nums: Vec<i32>) -> i32 {
+    let unique_elements = nums.iter().collect::<HashSet<_>>();
+
+    let mut majority: (i32, i32);
+
+    majority = (nums[0], 1);
+
+    for i in unique_elements {
+        let mut count = 0;
+
+        for j in &nums {
+            if i == j {
+                count += 1
+            }
+        }
+
+        if majority.1 < count {
+            majority = (*i, count)
+        }
+    }
+
+    majority.0
+}
+
 #[cfg(test)]
 mod tests {
     use std::vec;
@@ -419,5 +466,21 @@ mod tests {
         let output = 2;
 
         assert_eq!(output, string_construction(s));
+    }
+
+    #[test]
+    fn majority_element_case_1() {
+        let nums = vec![3, 2, 3];
+        let output = 3;
+
+        assert_eq!(output, majority_element(nums));
+    }
+
+    #[test]
+    fn majority_element_case_2() {
+        let nums = vec![2, 2, 1, 1, 1, 2, 2];
+        let output = 2;
+
+        assert_eq!(output, majority_element(nums));
     }
 }
