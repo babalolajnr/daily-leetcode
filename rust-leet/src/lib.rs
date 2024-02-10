bc2f15fde442ef0535096d27dffa420e8cf137ad
Abdulqudduus Babalola <babalolajnr@gmail.com>
2024-02-10 19:37:27 +0100

    solution: 10/02/24

--- rust-leet/src/lib.rs
+++ rust-leet/src/lib.rs
@@ -340,6 +340,46 @@ pub fn majority_element(nums: Vec<i32>) -> i32 {
     candidate
 }
 
+/// Given an integer array `nums`, rotate the array to the right by `k` steps,
+/// where `k` is **non-negative**.
+///
+///
+/// # Example 1:
+/// ```
+/// Input: nums = [1,2,3,4,5,6,7], k = 3
+/// Output: [5,6,7,1,2,3,4]
+/// Explanation:
+/// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+/// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+/// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+/// ```
+/// # Example 2:
+/// ```
+/// Input: nums = [-1,-100,3,99], k = 2
+/// Output: [3,99,-1,-100]
+/// Explanation:
+/// rotate 1 steps to the right: [99,-1,-100,3]
+/// rotate 2 steps to the right: [3,99,-1,-100]
+/// ```
+///
+/// # Constraints:
+///
+/// - `1 <= nums.length <= 105`
+/// - `-231 <= nums[i] <= 231 - 1`
+/// - `0 <= k <= 105`
+///
+/// # Follow up:
+///
+/// - Try to come up with as many solutions as you can. There are at least **three**
+/// different ways to solve this problem.
+/// - Could you do it in-place with O(1) extra space?
+pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+    let k = (k as usize) % nums.len();
+    nums.reverse();
+    nums[0..k].reverse();
+    nums[k..].reverse();
+}
+
 #[cfg(test)]
 mod tests {
     use std::vec;
@@ -476,5 +516,27 @@ mod tests {
         let output = 2;
 
         assert_eq!(output, majority_element(nums));
+    }
+
+    #[test]
+    fn rotate_case_1() {
+        let mut nums = vec![1, 2, 3, 4, 5, 6, 7];
+        let k = 3;
+        let output = vec![5, 6, 7, 1, 2, 3, 4];
+
+        rotate(&mut nums, k);
+
+        assert_eq!(output, nums);
+    }
+
+    #[test]
+    fn rotate_case_2() {
+        let mut nums = vec![-1, -100, 3, 99];
+        let k = 2;
+        let output = vec![3, 99, -1, -100];
+
+        rotate(&mut nums, k);
+
+        assert_eq!(output, nums);
     }
 }
